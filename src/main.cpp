5fda0058018f66060ba5ae764c6e00a2aab2565e
Juan David Barrero <JuandBarrero@hotmail.com>
2022-01-29 16:31:40 -0500

    motor brushless added

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,20 @@
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
+#include <Servo.h>
 
 Adafruit_MPU6050 mpu;
 sensors_event_t a, g, temp;
 
+Servo motorRigth;
+Servo motorLeft;
+
 void setup()
 {
   Serial.begin(115200);
+
+ motorRigth.attach(25);
+  motorLeft.attach(26);
 
   if (!mpu.begin())
   {
@@ -18,8 +25,15 @@ void setup()
       delay(10);
     }
   }
-
+  Serial.println("Sensor encontrado");
   mpu.setGyroRange(MPU6050_RANGE_250_DEG);
+
+  motorLeft.writeMicroseconds(1200);
+  motorRigth.writeMicroseconds(1200);
+  delay(5000);
+  Serial.println("Iniciando....");
+
+
 }
 
 void loop()
@@ -29,6 +43,8 @@ void loop()
 
   float accel_ang_y = atan(a.acceleration.y / sqrt(pow(a.acceleration.x, 2) + pow(a.acceleration.z, 2))) * (180.0 / 3.14);
 
+  motorRigth.writeMicroseconds(600);
+  motorLeft.writeMicroseconds(600);
   Serial.printf("El angulo en y es %4.2f \n", accel_ang_y);
 
   delay(100);
